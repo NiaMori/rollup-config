2ff497d53f729b94375811fd861fdfa9f8836812
NiaMori <Hellc5261@gmail.com>
2023-07-18 22:37:19 +0800

    fix: should still postprocess d.ts files when d.ts.map files do not exist

--- src/plugins.ts
+++ src/plugins.ts
@@ -16,35 +16,36 @@ export function createTypescriptDtsPostProcessorPlugin(props: { declarationDir: 
     name: '@niamori/rollup-config:typescript-dts-post-processor',
     writeBundle: async () => {
       for (const dts of await fg('**/*.d.ts', { cwd: declarationDir })) {
-        const dtsMap = dts.replace(/.d.ts$/, '.d.ts.map')
         const dmts = dts.replace(/.d.ts$/, '.d.mts')
-        const dmtsMap = dts.replace(/.d.ts$/, '.d.mts.map')
 
         const dtsPath = path.resolve(declarationDir, dts)
-        const dtsMapPath = path.resolve(declarationDir, dtsMap)
-
-        if (!await fs.promises.access(dtsMapPath).then(() => true).catch(() => false)) {
-          continue
-        }
-
         const dmtsPath = path.resolve(declarationDir, dmts)
-        const dmtsMapPath = path.resolve(declarationDir, dmtsMap)
 
         await fs.promises.rename(dtsPath, dmtsPath)
-        await fs.promises.rename(dtsMapPath, dmtsMapPath)
 
-        const dmtsSource = await fs.promises.readFile(dmtsPath, 'utf-8')
-        const dmtsSourceLastLine = dmtsSource.split('\n').pop()
+        const dtsMap = dts.replace(/.d.ts$/, '.d.ts.map')
+        const dtsMapPath = path.resolve(declarationDir, dtsMap)
 
-        if (dmtsSourceLastLine && dmtsSourceLastLine.startsWith('//# sourceMappingURL=')) {
-          const dmtsSourceWithoutLastLine = dmtsSource.split('\n').slice(0, -1).join('\n')
-          const dmtsSourceWithCorrectedSourceMappingURL = `${dmtsSourceWithoutLastLine}\n${dmtsSourceLastLine.replace(/.d.ts.map$/, '.d.mts.map')}`
-          await fs.promises.writeFile(dmtsPath, dmtsSourceWithCorrectedSourceMappingURL)
+        if (await fs.promises.access(dtsMapPath).then(() => true).catch(() => false)) {
+          const dmtsMap = dts.replace(/.d.ts$/, '.d.mts.map')
+          const dmtsMapPath = path.resolve(declarationDir, dmtsMap)
+
+          await fs.promises.rename(dtsMapPath, dmtsMapPath)
+
+          const dmtsSource = await fs.promises.readFile(dmtsPath, 'utf-8')
+          const dmtsSourceLastLine = dmtsSource.split('\n').pop()
+
+          if (dmtsSourceLastLine && dmtsSourceLastLine.startsWith('//# sourceMappingURL=')) {
+            const dmtsSourceWithoutLastLine = dmtsSource.split('\n').slice(0, -1).join('\n')
+            const dmtsSourceWithCorrectedSourceMappingURL = `${dmtsSourceWithoutLastLine}\n${dmtsSourceLastLine.replace(/.d.ts.map$/, '.d.mts.map')}`
+
+            const dmtsMapSource = await fs.promises.readFile(dmtsMapPath, 'utf-8')
+            const dmtsMapSourceWithCorrectedFile = dmtsMapSource.replace('.d.ts', '.d.mts')
+
+            await fs.promises.writeFile(dmtsPath, dmtsSourceWithCorrectedSourceMappingURL)
+            await fs.promises.writeFile(dmtsMapPath, dmtsMapSourceWithCorrectedFile)
+          }
         }
-
-        const dmtsMapSource = await fs.promises.readFile(dmtsMapPath, 'utf-8')
-        const dmtsMapSourceWithCorrectedFile = dmtsMapSource.replace('.d.ts', '.d.mts')
-        await fs.promises.writeFile(dmtsMapPath, dmtsMapSourceWithCorrectedFile)
       }
 
       await replaceTscAliasPaths({ outDir: declarationDir })
