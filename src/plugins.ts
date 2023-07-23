f0568d164af5d088e0053a295b950d7062f47241
NiaMori <Hellc5261@gmail.com>
2023-07-23 12:40:20 +0800

    feat: remove unused rollup-plugin-tsconfig-paths

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,8 +1,5 @@
-import createTsConfigPathsPluginCjsMod = require('rollup-plugin-tsconfig-paths')
 import createTypescriptPluginCjsMod = require('@rollup/plugin-typescript')
 
-export const createTsConfigPathsPlugin = createTsConfigPathsPluginCjsMod.default
 export const createTypescriptPlugin = createTypescriptPluginCjsMod.default
 
 export { default as createNodeExternalsPlugin } from 'rollup-plugin-node-externals'
-export { default as createDtsPlugin } from 'rollup-plugin-dts'
