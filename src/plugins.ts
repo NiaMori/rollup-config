4d2a701971573f489407ff217f08ef62949647f7
NiaMori <Hellc5261@gmail.com>
2023-07-23 12:45:08 +0800

    docs: add explanation about using cjs version of `@rollup/plugin-typescript`

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,3 +1,10 @@
+/**
+ * the esm exports of `@rollup/plugin-typescript` is [Masquerading as CJS](https://github.com/arethetypeswrong/arethetypeswrong.github.io/blob/main/docs/problems/FalseCJS.md)
+ * @see https://arethetypeswrong.github.io/?p=@rollup/plugin-typescript@11.1.2
+ *
+ * we have to use the cjs version util the issue is fixed
+ * @see https://github.com/rollup/plugins/issues/1541
+*/
 import createTypescriptPluginCjsMod = require('@rollup/plugin-typescript')
 
 export const createTypescriptPlugin = createTypescriptPluginCjsMod.default
